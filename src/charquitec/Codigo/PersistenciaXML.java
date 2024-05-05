7693a48cd3b3e224cd0c55d2b2d9ed7dc0af5c47
Sebastian <sebastian.peralta@unmsm.edu.pe>
2024-05-05 10:48:50 -0500

    Avance Persistencia

---
+++ src/charquitec/Codigo/PersistenciaXML.java
@@ -0,0 +1,15 @@
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
+
+import java.io.File;
+
+package charquitec.Codigo;
+
+public class PersistenciaXML {
+    Guardar(){
+        
+    }
+    Leer(){
+        
+    }
+}
