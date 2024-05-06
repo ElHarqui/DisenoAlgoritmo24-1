ba2da9d47ae47760c3739bb2c53935107b420a6e
Sebastian <sebastian.peralta@unmsm.edu.pe>
2024-05-06 11:49:49 -0500

    Persistencia Modificaciones

--- src/charquitec/Codigo/PersistenciaXML.java
+++ src/charquitec/Codigo/PersistenciaXML.java
@@ -69,3 +69,40 @@ public class PersistenciaXML {
     }
     */
 }
+/*
+    public void registroCliente(String nombre,String apellido, String codigo){
+        if(numDato < MAX){
+            Cliente ObjDato = new Cliente(nombre ,apellido,codigo);
+            GuardarCliente(ObjDato.toStringXML(),"DataClientes");
+            this.unCliente[numDato]=ObjDato;
+            numDato = numDato+1;
+        }else{
+            System.out.println("Limite de clientes sobrepasado");
+        }
+    }
+    public void GuardarDatosXML(String StringXML,String NombreArchivo){
+       PersistenciaXML Data = new PersistenciaXML("charquitec\\Data\\"+NombreArchivo+".xml");
+       Data.EscribirLineaXML(StringXML);
+       
+    }
+    public void LeerDatosXML(String NombreArchivo){     //Lee el archivo xml y lo guarda en clases como el metodo registroCliente() pero solo al iniciar el programa
+        this.numDato = 0;
+        PersistenciaXML Data = new PersistenciaXML("charquitec\\Data\\"+NombreArchivo+".xml");//Nombre de la ruta del archivo .xml
+        List<String> UsuariosLeidos = Data.LeerArchivoXML();  //Guardar cada linea en un espacio del List<String>
+        int tamano = UsuariosLeidos.size();                   //Obtener el largo del List<String>
+        String UsuarioLeido ; 
+        if (tamano < this.MAX){                               //Solo si no sobrepasa el maximo  se procede a crear los objetos tipo cliente y agregarlos al Lista de objetos
+            for (int i = 0; i < tamano; i++){
+               UsuarioLeido = UsuariosLeidos.get(i);
+               String[] DataUsuario = UsuarioLeido.split(";");
+               Cliente ObjDato = new Cliente(DataUsuario[1] ,DataUsuario[2],DataUsuario[0]);
+               this.unCliente[numDato]=ObjDato;
+               numDato = numDato+1;        
+
+           }           
+        }else{
+            System.out.println("Limite de usuarios sobrepasado");
+        }
+    }
+
+*/
